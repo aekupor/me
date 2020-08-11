5214bef4090c24e3a9cfc9b6120ecfb1ea1be4da
Ashlee <aekupor@gmail.com>
2020-08-11 09:20:35 -0700

    Bug fixes to get website to run

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
 import React, { Component } from 'react';
 import './App.css';
 import Introduction from './components/introduction'
 
-function App() extends Component {
-  return (
-    <div>
-			<Introduction></Introduction>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+  			<Introduction></Introduction>
+      </div>
+    );
+  }
 }
 
 export default App;
--- website/src/components/introduction.jsx
+++ website/src/components/introduction.jsx
@@ -6,7 +6,8 @@ export default class Introduction extends Component {
       <div>
         <h1>Hi! <br />I'm Ashlee</h1>
         <h1>I love building<br /> THINGS !!</h1>
-        <p><a href="https://wwwgoogle.com" target="_blank" rel="noopener noreferrer">View Resume</a></p>
+        <p><a href="https://www.google.com" target="_blank" rel="noopener noreferrer">View Resume</a></p>
       </div>
     )
+  }
 }
