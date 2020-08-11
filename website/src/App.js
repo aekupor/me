916997111f5bcab518d787214c60fabe58b7e5b8
Ashlee <aekupor@gmail.com>
2020-08-11 09:25:39 -0700

    Create about me and project classes.

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Introduction from './components/introduction'
+import About from './components/about'
+import Projects from './components/projects'
 
 class App extends Component {
   render() {
     return (
       <div>
   			<Introduction></Introduction>
+        <About></About>
+        <Projects></Projects>
       </div>
     );
   }
