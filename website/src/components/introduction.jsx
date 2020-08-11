aeabaf5dfa70305556a61e50898d8ab771e758ea
Ashlee <aekupor@gmail.com>
2020-08-11 09:17:28 -0700

    Create introduction file.

---
+++ website/src/components/introduction.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi! <br />I'm Ashlee</h1>
+        <h1>I love building<br /> THINGS !!</h1>
+        <p><a href="https://wwwgoogle.com" target="_blank" rel="noopener noreferrer">View Resume</a></p>
+      </div>
+    )
+}
