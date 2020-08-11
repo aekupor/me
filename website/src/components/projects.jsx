cfa82078597c4d7a19825f179f9fada01555a1d5
Ashlee <aekupor@gmail.com>
2020-08-11 12:50:07 -0700

    Lay out cards in row.

--- website/src/components/projects.jsx
+++ website/src/components/projects.jsx
@@ -10,19 +10,42 @@ export default class Projects extends Component {
           <h2>My projects</h2>
         </div>
 
-        <div class="card">
-          <img class="logo" src={logo} alt="Logo" />
-          <div class="container">
-            <h4><b>Project 1</b></h4>
-            <p>More about project</p>
+        <div class="row">
+          <div class="column">
+            <div class="card">
+              <img class="logo" src={logo} alt="Logo" />
+              <div class="container">
+                <h4><b>Project 1</b></h4>
+                <p>More about project</p>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div class="card">
-          <img class="logo" src={logo} alt="Logo" />
-          <div class="container">
-            <h4><b>Project 2</b></h4>
-            <p>More about project</p>
+          <div class="column">
+            <div class="card">
+              <img class="logo" src={logo} alt="Logo" />
+              <div class="container">
+                <h4><b>Project 2</b></h4>
+                <p>More about project</p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <img class="logo" src={logo} alt="Logo" />
+              <div class="container">
+                <h4><b>Project 3</b></h4>
+                <p>More about project</p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <img class="logo" src={logo} alt="Logo" />
+              <div class="container">
+                <h4><b>Project 4</b></h4>
+                <p>More about project</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
