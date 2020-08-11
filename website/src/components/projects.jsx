a23859d54cbd00028e3a353ede21f63e09c673c7
Ashlee <aekupor@gmail.com>
2020-08-11 09:38:13 -0700

    Add project stylesheet.

--- website/src/components/projects.jsx
+++ website/src/components/projects.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import '../css/project.css'
 
 export default class Projects extends Component {
   render() {
     return (
       <div>
-        <p>My projects</p>
+        <h2>My projects</h2>
+
+        <span className="project">This is my first project.</span>
+        <span className="project">This is my second project.</span>
+        <span className="project">This is my third project.</span>
       </div>
     )
   }
