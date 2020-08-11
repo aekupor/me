8ecfea76dfa0d110ae87654c89b883ea2bbc3202
Ashlee <aekupor@gmail.com>
2020-08-11 15:59:25 -0700

    Fix resume download bug and credesign button

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,22 +1,16 @@
 import React, { Component } from 'react'
 import '../css/buttons.css'
+import Resume from '../images/Resume.pdf';
 
 export default class Introduction extends Component {
+
   render() {
     return (
       <div>
         <h1>Hi! <br />I'm Ashlee</h1>
         <h1>I love building<br /> THINGS !!</h1>
 
-        <div>
-          <p>
-            {
-              //TODO: when click, says "Failed - no file"
-            }
-            <a href="./../images/Resume.pdf" class="button" download="Ashlee_Kupor_Resume">Download Resume</a>
-          </p>
-       </div>
-       <button class="buttons">Hey There</button>
+        <a href={Resume} class="buttons" download="Ashlee_Kupor_Resume">Download Resume</a>
       </div>
     )
   }
