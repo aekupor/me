1863b65594adda02d512719d377bafcd6a3b0611
Ashlee <aekupor@gmail.com>
2020-08-11 17:05:14 -0700

    Add fade in effect

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import '../css/buttons.css'
 import Resume from '../images/Resume.pdf';
+import Fade from 'react-reveal/Fade';
 
 export default class Introduction extends Component {
 
   render() {
     return (
       <div>
+      <Fade bottom>
         <h1>Hi! <br />I'm Ashlee</h1>
         <h1>I love building<br /> THINGS !!</h1>
 
         <a href={Resume} class="buttons" download="Ashlee_Kupor_Resume">Download Resume</a>
+      </Fade>
       </div>
     )
   }
