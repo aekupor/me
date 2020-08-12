98ada6ca5143bfcdf778dfd432a1748c2562bf0f
Ashlee <aekupor@gmail.com>
2020-08-11 21:07:44 -0700

    Fix merge conflict

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
 import '../css/navbar.css'
 
-<<<<<<< HEAD
-export default class Navbar extends Component{
-=======
-export default class MyNavbar extends Component{
->>>>>>> 88b64fc15b161dd5f2f106e13f6068337349e0cd
+export default class Navbar extends Component {
   render() {
       return (
         <div class = "navbar">
